cda88122f86629a8bbae2c0d9a8ab18d34435c78
geon <gunchung78@gmail.com>
2024-04-18 13:59:56 +0900

    [PGS] 오픈채팅방 / lv2 / 60분 / 70.52ms / 이건

--- Week2/08_해시_240416/PGS_오픈채팅방/이건.py
+++ Week2/08_해시_240416/PGS_오픈채팅방/이건.py
@@ -8,4 +8,20 @@
 ## 분
 
 # 실행시간 
-## ms~ ms
+## 0.02ms~ 70.52ms
+def solution(record):
+    answer = []
+    dic = {}
+    for i in record:
+        arr = i.split(" ")
+        if arr[0] != "Leave":
+            dic[arr[1]] = arr[2]
+            
+    for i in record:
+        arr = i.split(" ")
+        if arr[0] == "Enter":
+            answer.append(dic[arr[1]] + "님이 들어왔습니다.")
+        elif arr[0] == "Leave":
+            answer.append(dic[arr[1]] + "님이 나갔습니다.")
+        
+    return answer
